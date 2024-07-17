c0e1dd88d83c10f6278c3b8808a45e0468e55dfd
UjanGX <vidiox060@gmail.com>
2024-07-18 06:16:01 +0700

    Delete vidi.js

--- vidi.js
+++
@@ -1,6 +0,0 @@
-const menu = document.getElementById('menu-label');
-const sidebar = document.getElementsByClassName('sidebar')[0];
-
-menu.addEventListener('click', function() {
-    sidebar.classList.toggle('hide');
-})
